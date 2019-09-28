83e1cd92aef8d874992eee370eac8fcadf7c45bb
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-28 03:49:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,9 +3,11 @@ require_relative '../lib/concerns/memorable.rb'
 require_relative '../lib/concerns/findable.rb'
 require_relative '../lib/concerns/paramable.rb'
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
+  
   attr_accessor :name
   attr_reader :songs
 
