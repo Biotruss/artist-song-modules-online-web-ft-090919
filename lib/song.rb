a11d6e2c713c3181276c33aefe3fc268aeac45fd
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-28 03:49:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ require_relative '../lib/concerns/memorable.rb'
 require_relative '../lib/concerns/findable.rb'
 require_relative '../lib/concerns/paramable.rb'
 class Song
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
   
